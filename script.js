2546b321a826b75b7788f2ce176e4e0028632ce6
Magnus Holm <magnusthestrup@hotmail.com>
2023-05-22 15:53:42 +0200

    All pages done, just missing footer + compressed BG video + .htaccess

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function OpenCloseMobileMenu() {
-  let menu = document.querySelector('#menu-icon');
-  let navbar = document.querySelector('.navbar');
-
-  menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+function mobileMenu() {
+  let menu = document.querySelector("#mobile_overlay");
+  let body = document.querySelector("body");
+  if (menu.style.width === "100%") {
+    menu.style.width = "0";
+    body.style.overflow = "visible";
+  } else {
+    menu.style.width = "100%";
+    body.style.overflow = "hidden";
   }
 }
