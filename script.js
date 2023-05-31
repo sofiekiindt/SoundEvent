f289854549812ac367f1b821ef978eaac0a26806
Magnus Holm <magnusthestrup@hotmail.com>
2023-05-31 16:07:47 +0200

    Files updated to match online version

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ window.addEventListener("scroll", () => {
   // Dividere "scroll" med et decimal tal, så "scroll" bliver størrer og gemmer det som variablen "opacity"
   let opacity = scroll / 0.4;
 
-  // Checker om "opacity" er over 0.7 og hvis den er bliver den sat til 0.7. Det gør at siden ikke bliver for mørk på lange sider.
-  if (opacity > 0.7) {
-    opacity = 0.7;
+  // Checker om "opacity" er over 0.6 og hvis den er bliver den sat til 0.6. Det gør at siden ikke bliver for mørk på lange sider.
+  if (opacity > 0.6) {
+    opacity = 0.6;
   }
 
   // Finder #background_darkener i HTML filen og gemmer den som en variable
