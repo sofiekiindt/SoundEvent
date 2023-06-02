93ccc95801ffdeccf76c8a2ef96262ad9cf0fb25
Magnus Holm <magnusthestrup@hotmail.com>
2023-06-02 14:15:19 +0200

    Several asset changes

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ function mobileMenu() {
   // Her checkes om #mobile_overlay har en width på 100%
   if (menu.style.width === "100%") {
 
-    // Hvis #mobile_overlay har en width på 100%, sættes dens width til 0, hvilket for mobil menuen til at forsvinde, og body får overflow = "visible", hvilket gør at man kan scoll på siden
+    /* Hvis #mobile_overlay har en width på 100%, sættes dens width til 0, hvilket for mobil menuen 
+    til at forsvinde, og body får overflow = "visible", hvilket gør at man kan scoll på siden */
     menu.style.width = "0";
     body.style.overflow = "visible";
   } else {
 
-    // Hvis #mobile_overlay har en width på 0, sættes dens width til 100%, hvilket for mobil menuen til at blive vist og body får overflow = "hidden", hvilket gør at man ikke kan scoll på siden
+    /* Hvis #mobile_overlay har en width på 0, sættes dens width til 100%, hvilket for mobil menuen 
+    til at blive vist og body får overflow = "hidden", hvilket gør at man ikke kan scoll på siden */
     menu.style.width = "100%";
     body.style.overflow = "hidden";
   }
@@ -36,6 +38,7 @@ window.addEventListener("scroll", () => {
   // Finder #background_darkener i HTML filen og gemmer den som en variable
   let background_darkener = document.querySelector("#background_darkener");
 
-  // Opdatere alpha værdien for #background_darkener's baggrundsfarve til værdien gemt i "opacity". Det gør at baggrunds videoen bliver mere eller mindre synlig.
+  /* Opdatere alpha værdien for #background_darkener's baggrundsfarve til værdien gemt i "opacity". 
+  Det gør at baggrunds videoen bliver mere eller mindre synlig. */
   background_darkener.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
 })
